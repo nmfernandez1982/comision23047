0ee90ea5de3fedd38dee8afc9daf6a48e46034ee
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2023-05-22 02:46:21 -0300

    clase22

---
+++ Clase22/scripts.sql
@@ -0,0 +1,28 @@
+/*
+
+Una base de datos relacional 
+define un conjunto de reglas que se deben respetar para almacenar la información que se necesite.
+
+
+Objetos que componen una db
+Tablas--> conunto de datos 
+Vistas--
+Funciones--> propias/del sistema
+Lenguaje---->SQL es un lenguaje de programacion. DML/DLL
+
+MYSQL/MariaDB
+SQL Server
+ORACLE
+PostgreSQL/Mongo DB/ACCESS
+
+
+*/
+
+create database prueba47;
+
+drop database prueba47;
+
+
+
+
+
